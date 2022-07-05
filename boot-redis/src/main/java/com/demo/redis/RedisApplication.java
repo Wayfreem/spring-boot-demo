09fcd4acdd62d7126a0fdd3087037732c73ffce1
WuQuan <wuq@apestek.com>
2022-07-05 19:43:12 +0800

    增加 redis 模块

---
+++ boot-redis/src/main/java/com/demo/redis/RedisApplication.java
@@ -0,0 +1,17 @@
+package com.demo.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuq
+ * @Time 2022-7-5 19:38
+ * @Description
+ */
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
