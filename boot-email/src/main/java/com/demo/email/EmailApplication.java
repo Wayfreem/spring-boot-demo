fe3311ab0e993e960ca3e7208092ba6674397946
WuQuan <wuq@apestek.com>
2022-07-05 18:36:44 +0800

    增加 boot-email 模块

---
+++ boot-email/src/main/java/com/demo/email/EmailApplication.java
@@ -0,0 +1,17 @@
+package com.demo.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuq
+ * @Time 2022-7-5 16:28
+ * @Description
+ */
+@SpringBootApplication
+public class EmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailApplication.class, args);
+    }
+}
