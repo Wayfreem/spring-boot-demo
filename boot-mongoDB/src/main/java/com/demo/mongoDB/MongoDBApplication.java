7a4c158eaa0d0b65dd36fb15f7ad4115c13f28e5
WuQuan <wuq@apestek.com>
2022-07-14 14:48:30 +0800

    增加 MongoDB 部分

---
+++ boot-mongoDB/src/main/java/com/demo/mongoDB/MongoDBApplication.java
@@ -0,0 +1,17 @@
+package com.demo.mongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuq
+ * @Time 2022-7-14 14:13
+ * @Description
+ */
+@SpringBootApplication
+public class MongoDBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDBApplication.class, args);
+    }
+}
