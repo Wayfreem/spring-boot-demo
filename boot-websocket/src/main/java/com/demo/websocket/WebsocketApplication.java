d2eff3de0214c2baae33710e883192bd9349cc8c
WuQuan <wuq@apestek.com>
2022-07-22 13:57:45 +0800

    增加启动类

---
+++ boot-websocket/src/main/java/com/demo/websocket/WebsocketApplication.java
@@ -0,0 +1,17 @@
+package com.demo.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuq
+ * @Time 2022-7-22 13:37
+ * @Description
+ */
+@SpringBootApplication
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+}
