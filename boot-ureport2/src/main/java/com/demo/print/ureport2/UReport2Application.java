6b4956b09f336fa960be2c28216a6c42d50db96a
Wayfreem <wayfreem@163.com>
2022-07-23 15:27:19 +0800

    搭建 ureport2 工程

---
+++ boot-ureport2/src/main/java/com/demo/print/ureport2/UReport2Application.java
@@ -0,0 +1,12 @@
+package com.demo.print.ureport2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UReport2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UReport2Application.class, args);
+    }
+}
