ac04d950d3d4bc0fc8531367633ea18c7302a044
Wayfreem <wayfreem@163.com>
2022-07-23 16:03:15 +0800

    搭建 ureport2 工程

--- boot-ureport2/README.md
+++ boot-ureport2/README.md
@@ -1,4 +1,7 @@
 ## 简介
+
+[ureport2 学习地址](https://www.w3cschool.cn/ureport/)
+
 使用 spring boot 集成 UReport2 实现打印报表功能开发。
 
 既然是需要连接数据库，就需要安装 MySQL(采用docker 安装) [安装参考链接](https://blog.csdn.net/qq_18948359/article/details/125486934?spm=1001.2014.3001.5502)
@@ -6,6 +9,15 @@
 ## 集成的步骤
 
 ### 第一步：引入依赖
+
+pom 文件
+```xml
+<dependency>
+    <groupId>com.bstek.ureport</groupId>
+    <artifactId>ureport2-console</artifactId>
+    <version>2.2.9</version>
+</dependency>
+```
 
 ### 第二步：
 
---
+++ boot-ureport2/src/main/java/com/demo/print/ureport2/config/UReport2Config.java
@@ -0,0 +1,21 @@
+package com.demo.print.ureport2.config;
+
+import com.bstek.ureport.console.UReportServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:ureport-console-context.xml")
+public class UReport2Config {
+
+    /**
+     * 这里是采用 ServletRegistrationBean 向 spring 容器创建一个 servlet 服务
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean buildUReportServlet() {
+        return new ServletRegistrationBean(new UReportServlet(), "/ureport/*");
+    }
+}
