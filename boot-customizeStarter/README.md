853d5f34f438808855df5ca542d06a3e3d3ed858
Wayfreem <wayfreem@163.com>
2022-08-11 21:50:40 +0800

    增加自定义 starter 模块

---
+++ boot-customizeStarter/README.md
@@ -0,0 +1,6 @@
+## 工程说明
+
+这里是自定义个 spring boot starter，更好的掌握 spring boot starter 的知识。了解 spring boot starter 的加载机制。
+
+## 集成说明
+
---
+++ boot-customizeStarter/src/main/java/com/demo/customizeStarter/CustomizeStarterApplication.java
@@ -0,0 +1,12 @@
+package com.demo.customizeStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomizeStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomizeStarterApplication.class, args);
+    }
+}
