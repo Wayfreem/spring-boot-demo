f1a7f359514dd79a067000951109f810e101766c
Wayfreem <wayfreem@163.com>
2022-08-17 21:29:59 +0800

    增加 actuator 项目

---
+++ boot-actuator/README.md
@@ -0,0 +1,6 @@
+## 简介
+
+spring boot 中集成 actuator 实现监控，运维管理
+
+## 集成步骤
+
---
+++ boot-actuator/src/main/java/com/demo/actuator/ActuatorApplication.java
@@ -0,0 +1,8 @@
+package com.demo.actuator;
+
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
