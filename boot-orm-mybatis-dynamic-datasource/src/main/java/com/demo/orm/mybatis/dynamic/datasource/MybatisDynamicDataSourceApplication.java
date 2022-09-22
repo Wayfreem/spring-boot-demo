c833ca2925699be07cf74f0055a95b0396a2e9a5
Wayfreem <wayfreem@163.com>
2022-09-22 21:28:29 +0800

    增加基于 mybatis 实现的多数据源操作模块

---
+++ boot-orm-mybatis-dynamic-datasource/src/main/java/com/demo/orm/mybatis/dynamic/datasource/MybatisDynamicDataSourceApplication.java
@@ -0,0 +1,11 @@
+package com.demo.orm.mybatis.dynamic.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisDynamicDataSourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisDynamicDataSourceApplication.class, args);
+    }
+}
