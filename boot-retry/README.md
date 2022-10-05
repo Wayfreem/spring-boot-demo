5e80b7c1e2c11e87e678c2cd0c26ba06b31a7cd1
Wayfreem <wayfreem@163.com>
2022-10-05 15:56:31 +0800

    增加 retry 工程

---
+++ boot-retry/README.md
@@ -0,0 +1,14 @@
+## 说明
+
+Spring Retry 为 Spring 应用程序提供了声明性重试支持。
+
+## 集成说明
+
+### 第一步：引入依赖
+
+```xml
+<dependency>
+    <groupId>org.springframework.retry</groupId>
+    <artifactId>spring-retry</artifactId>
+</dependency>
+```
---
+++ boot-retry/src/main/java/com/demo/retry/RetryApplication.java
@@ -0,0 +1,13 @@
+package com.demo.retry;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetryApplication.class, args);
+    }
+}
