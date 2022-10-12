a263b07ec31894e334b2d73aebd5d837956949b5
Wayfreem <wayfreem@163.com>
2022-10-12 22:29:59 +0800

    增加 build 部分

--- boot-servlet-filter/src/main/java/com/demo/servlet/filter/FilterApplication.java
+++ boot-servlet-filter/src/main/java/com/demo/servlet/filter/FilterApplication.java
@@ -1,13 +1,18 @@
 package com.demo.servlet.filter;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author wuq
  * @Time 2022-10-12 17:07
  * @Description
  */
+@SpringBootApplication
 public class FilterApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(FilterApplication.class, args);
     }
 }
