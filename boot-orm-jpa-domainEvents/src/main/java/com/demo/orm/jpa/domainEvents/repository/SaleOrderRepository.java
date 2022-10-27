a44dea9feaa1a01e8934dfe49adac4c1330ee177
Wayfreem <wayfreem@163.com>
2022-10-27 11:21:39 +0800

    增加内容

--- boot-orm-jpa-domainEvents/src/main/java/com/demo/orm/jpa/domainEvents/repository/SaleOrderRepository.java
+++ boot-orm-jpa-domainEvents/src/main/java/com/demo/orm/jpa/domainEvents/repository/SaleOrderRepository.java
@@ -1,6 +1,6 @@
 package com.demo.orm.jpa.domainEvents.repository;
 
-import com.demo.test.jpa.model.SaleOrder;
+import com.demo.orm.jpa.domainEvents.model.SaleOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SaleOrderRepository extends JpaRepository<SaleOrder, Long> {
