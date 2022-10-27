3ac5ee34112ae6377b08136e62e33fb55fca282f
Wayfreem <wayfreem@163.com>
2022-10-27 10:56:13 +0800

    增加启动类

--- boot-orm-jpa-domainEvents/src/main/java/com/demo/orm/jpa/domainEvents/JpaDomainEvents.java
+++ boot-orm-jpa-domainEvents/src/main/java/com/demo/orm/jpa/domainEvents/JpaDomainEvents.java
@@ -1,8 +1,12 @@
 package com.demo.orm.jpa.domainEvents;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class JpaDomainEvents {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(JpaDomainEvents.class);
     }
 }
