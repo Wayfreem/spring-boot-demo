d9e6d7bce3ba3225e2f9072c8a1014befd4ed77f
WuQuan <wayfreem@163.com>
2023-01-11 09:36:05 +0800

    增加启动类内容

--- boot-sse/src/main/java/com/demo/sse/SSEApplication.java
+++ boot-sse/src/main/java/com/demo/sse/SSEApplication.java
@@ -1,4 +1,19 @@
 package com.demo.sse;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * @author wuq
+ * @Time 2022-12-16 16:53
+ * @Description
+ */
+@SpringBootApplication
+@ServletComponentScan
 public class SSEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SSEApplication.class, args);
+    }
 }
