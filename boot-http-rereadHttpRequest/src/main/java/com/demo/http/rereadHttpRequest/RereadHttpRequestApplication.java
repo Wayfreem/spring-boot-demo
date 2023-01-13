d2f9635c10612034fa5ed941fc684177f7307ae7
WuQuan <wayfreem@163.com>
2023-01-13 09:14:38 +0800

    增加重复读取 request Body 模块

---
+++ boot-http-rereadHttpRequest/src/main/java/com/demo/http/rereadHttpRequest/RereadHttpRequestApplication.java
@@ -0,0 +1,17 @@
+package com.demo.http.rereadHttpRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuq
+ * @Time 2023-1-12 16:17
+ * @Description
+ */
+@SpringBootApplication
+public class RereadHttpRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RereadHttpRequestApplication.class, args);
+    }
+}
