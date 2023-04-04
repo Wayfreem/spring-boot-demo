2b5e2209a4b4382bb67c8c11e405cbc567d925bc
WuQuan <wayfreem@163.com>
2023-04-04 14:38:10 +0800

    增加 druid 项目

---
+++ boot-druid/README.md
@@ -0,0 +1,4 @@
+## 说明
+
+在 Spring Boot 中集成 druid 实现 SQL 监控。[参考文档](https://mp.weixin.qq.com/s/bXuy7lkPJYtcWhbrXQPnjg)
+
---
+++ boot-druid/src/main/java/com/demo/druid/DruidApplication.java
@@ -0,0 +1,14 @@
+package com.demo.druid;
+
+/**
+ * Druid 集成
+ *
+ * @author wuq
+ * @Time 2023-4-4 14:34
+ * @Description
+ */
+public class DruidApplication {
+    public static void main(String[] args) {
+
+    }
+}
