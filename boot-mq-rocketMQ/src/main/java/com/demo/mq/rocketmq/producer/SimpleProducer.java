9cd8a90baa41feef8c95362e1bebc26822f2c5af
WuQuan <wayfreem@163.com>
2024-07-11 21:45:42 +0800

    增加发送消息的类型

--- boot-mq-rocketMQ/src/main/java/com/demo/mq/rocketmq/producer/SimpleProducer.java
+++ boot-mq-rocketMQ/src/main/java/com/demo/mq/rocketmq/producer/SimpleProducer.java
@@ -1,9 +1,12 @@
 package com.demo.mq.rocketmq.producer;
 
+import com.alibaba.fastjson2.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Component;
  * @Time 2023-5-5 14:12
  * @Description
  */
+@Slf4j
 @Component
 public class SimpleProducer {
 
@@ -25,7 +29,19 @@ public class SimpleProducer {
      */
     public void sendSyncMsg(String topic, String msg) {
         rocketMQTemplate.convertAndSend(topic, msg);
+//        rocketMQTemplate.send(topic + ":tag1", MessageBuilder.withPayload(user).build()); // 等价于上面一行
     }
+
+    /**
+     * 发送同步消息（阻塞当前线程，等待broker响应发送结果，这样不太容易丢失消息）
+     * （msgBody也可以是对象，sendResult为返回的发送结果）
+     */
+    public SendResult sendMsg(String topic, String msg) {
+        SendResult sendResult = rocketMQTemplate.syncSend(topic, MessageBuilder.withPayload(msg).build());
+        log.info("【sendMsg】sendResult={}", JSON.toJSONString(sendResult));
+        return sendResult;
+    }
+
 
     /**
      * 异步消息
@@ -58,5 +74,43 @@ public class SimpleProducer {
      */
     public void sendAsyncMsg(String topic, String msg, SendCallback sendCallback) {
         rocketMQTemplate.asyncSend(topic, msg, sendCallback);
+    }
+
+    /**
+     * 发送延时消息（上面的发送同步消息，delayLevel的值就为0，因为不延时）
+     * 在start版本中 延时消息一共分为18个等级分别为：1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h
+     *
+     * @param topic          主题
+     * @param msgBody        消息体
+     * @param messageTimeOut 消息超时时间
+     * @param delayLevel     延时等级
+     */
+    public void sendDelayMsg(String topic, String msgBody, int messageTimeOut, int delayLevel) {
+        rocketMQTemplate.syncSend(topic, MessageBuilder.withPayload(msgBody).build(), messageTimeOut, delayLevel);
+    }
+
+
+    /**
+     * 发送单向消息
+     *
+     * @param topic 主题
+     * @param msg   消息体
+     */
+    public void sendOneWayMsg(String topic, String msg) {
+        rocketMQTemplate.sendOneWay(topic, MessageBuilder.withPayload(msg).build());
+    }
+
+
+    /**
+     * 发送带tag的消息，直接在topic后面加上":tag"
+     *
+     * @param topic   主题
+     * @param tag     标签
+     * @param msg 消息体
+     * @return SendResult
+     */
+    public SendResult sendTagMsg(String topic, String tag, String msg) {
+        return rocketMQTemplate.syncSend(topic + ":" + tag, MessageBuilder.withPayload(msg).build());
+//        return rocketMQTemplate.syncSend(topic + ":tag2", MessageBuilder.withPayload(msg).build());
     }
 }
