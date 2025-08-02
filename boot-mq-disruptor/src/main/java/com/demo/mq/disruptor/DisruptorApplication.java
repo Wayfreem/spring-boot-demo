68f61f3c4a88931fde2e6e0fe5096e9d901d680f
wuquan <zhisucang_wuquan@1039594575973476.onaliyun.com>
2025-08-02 15:09:26 +0800

    完善项目

--- boot-mq-disruptor/src/main/java/com/demo/mq/disruptor/DisruptorApplication.java
+++ boot-mq-disruptor/src/main/java/com/demo/mq/disruptor/DisruptorApplication.java
@@ -1,9 +1,17 @@
 package com.demo.mq.disruptor;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author wuq
  * @Time 2023-6-13 9:44
  * @Description
  */
+@SpringBootApplication
 public class DisruptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisruptorApplication.class, args);
+    }
 }
